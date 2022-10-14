546e48ca3d23e175736c8efdc0b15410cad3a11b
indodesigner <webdesigner@indocosmo.com>
2022-10-14 16:08:21 +0530

    edits

--- src/component/NewReservation/RoomAvailability.js
+++ src/component/NewReservation/RoomAvailability.js
@@ -4,30 +4,30 @@ function RoomAvailability() {
    
     return (
         <div className="row p-3">
-        <div className="col-sm-3 p-3 availability_check_bg">
+        <div className="col-md-3 availability_check_bg">
                 <div className="col-sm-12">
-                    <div className="form-group inputfieldpadding">
-                        <label className="text-left" htmlFor="arrival">Arrival :</label>
+                    <div className="form-group">
+                        <label className="text-left" htmlFor="arrival">Arrival:</label>
                             <input type="datetime-local" className="form-control arrival" name="arrival" id="arrival"/>
                     </div>
-                    <div className="form-group inputfieldpadding">
-                        <label className="text-left" htmlFor="departure">Departure :</label>
+                    <div className="form-group">
+                        <label className="text-left" htmlFor="departure">Departure:</label>
                             <input type="datetime-local" className="form-control departure" name="departure" id="departure"/>
                     </div>
                 </div>
-                <div className="col-sm-12 p-1">   
+                <div className="col-sm-12">   
                     <div className="row">
-                        <div className="col-sm-6 inputfieldpadding">
+                        {/* <div className="col-sm-6"> */}
+                            <div className="form-group">
                             <label className="text-left" htmlFor="nights">Nights</label>
-                            <div className="form-group">
                                 <input type="number"  className="form-control"  placeholder="Select Nights count"  min="1" name="nights" id="nights"  />
-                            </div>
+                            {/* </div> */}
                         </div>
-                         <div className="col-sm-6 inputfieldpadding">
-                            <label className="text-left" htmlFor="rooms">Rooms</label>
+                         {/* <div className="col-sm-6"> */}
                             <div className="form-group">
+                            <label className="text-left" htmlFor="rooms">Rooms</label>
                                 <input type="number"  className="form-control"  placeholder="Select Rooms count" min="1"  name="rooms" id="rooms"  />
-                            </div>
+                            {/* </div> */}
                         </div>
                     </div>
                 </div>
@@ -36,11 +36,11 @@ function RoomAvailability() {
                     <button type="button" className="btn btn-success w-25 btnstyle">Go</button>
                 </div>
         </div>
-        <div className="col-sm-9">
+        <div className="col-md-9">
         <div className= "roomavailability">
             <div className='m-2'>
-            <table className="table" style={{marginBottom: 0,}}>
-                <thead className="tabhead">
+            <table className="table avail-table" style={{marginBottom: 0,}}>
+                <thead className="tabhead head-bg">
                     <tr>
                         <th>Room Type</th>
                         <th><span className='avail-date'>16-Sep-22</span><br/><span className='avail-date-head'> Fri </span></th>
@@ -49,11 +49,21 @@ function RoomAvailability() {
                         <th><span className='avail-date'>19-Sep-22</span><br/><span className='avail-date-head'> Mon </span></th>
                         <th><span className='avail-date'>20-Sep-22</span><br/><span className='avail-date-head'> Tue </span></th>
                         <th><span className='avail-date'>21-Sep-22</span><br/><span className='avail-date-head'> Wed </span></th>
+                        <th><span className='avail-date'>22-Sep-22</span><br/><span className='avail-date-head'> Thu </span></th>
+                        <th><span className='avail-date'>23-Sep-22</span><br/><span className='avail-date-head'> Fri </span></th>
+                        <th><span className='avail-date'>24-Sep-22</span><br/><span className='avail-date-head'> Sat </span></th>
+                        <th><span className='avail-date'>25-Sep-22</span><br/><span className='avail-date-head'> Sun </span></th>
+                        <th><span className='avail-date'>26-Sep-22</span><br/><span className='avail-date-head'> Mon </span></th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className='avail-tbody'>
                     <tr>
                         <td>DLX</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
                         <td>6</td>
                         <td>6</td>
                         <td>6</td>
@@ -69,9 +79,19 @@ function RoomAvailability() {
                         <td>6</td>
                         <td>6</td>
                         <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
                     </tr>
                     <tr>
                         <td>CLASS</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
                         <td>6</td>
                         <td>6</td>
                         <td>6</td>
@@ -87,9 +107,19 @@ function RoomAvailability() {
                         <td>6</td>
                         <td>6</td>
                         <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
                     </tr>
                     <tr>
                         <td>STND</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
+                        <td>6</td>
                         <td>6</td>
                         <td>6</td>
                         <td>6</td>
@@ -100,8 +130,8 @@ function RoomAvailability() {
                 </tbody>
             </table>
                 <div class="d-flex justify-content-end">
-                    <button type="button" className="btn"><i class="bi bi-chevron-left"></i></button>
-                    <button type="button" className="btn"><i class="bi bi-chevron-right"></i></button>
+                    <button type="button" className="btn"><i class="bi bi-chevron-left me-1"></i>Prev</button>
+                    <button type="button" className="btn">Next<i class="bi bi-chevron-right ms-1"></i></button>
                 </div>
             </div>
             </div>
