e88dfb6f80f2e4f760a186dafd272abb1923b6ec
indodesigner <webdesigner@indocosmo.com>
2022-10-17 14:46:13 +0530

    edits

--- src/component/NewReservation/ReservedBy.js
+++ src/component/NewReservation/ReservedBy.js
@@ -2,45 +2,43 @@ import React, { Component } from 'react';
 
 function ReservedBy() {
     return(
-        <div className="col-sm-6" >
+        <div className="col-12" >
                 <div>
-                        <ul className="ulistmargin">
+                        {/* <ul> */}
                             <div>
-                                <h4 className="label"> RESERVED BY </h4>
+                                {/* <h4> RESERVED BY </h4> */}
                             </div>
                             <div className="border-bottom">
                                 <div className="row">
                                     <div className="col-sm-4">
                                         <div className="inputfieldpadding">
-                                            <label className="text-left">Title</label>
-                                            <select className="form-control m-bot15">
-                                                <option>Option 1</option>
-                                                <option>Option 2</option>
-                                                <option>Option 3</option>
-                                            </select>
+                                            <label className="text-left"></label>
+                                            <input type="text" className="form-control" placeholder="Full Name" />
                                         </div>
                                     </div>
                                     <div className="col-sm-4">
                                         <div className="inputfieldpadding">
-                                            <label className="text-left">First Name</label>
-                                            <input type="text" className="form-control" placeholder="" />
+                                            <label className="text-left"></label>
+                                            <input type="text" className="form-control" placeholder="Phone Number" />
                                         </div>
                                     </div>
                                     <div className="col-sm-4">
                                         <div className="inputfieldpadding">
-                                            <label className="text-left">Last Name</label>
-                                            <input type="text" className="form-control" placeholder="" />
+                                            <label className="text-left"></label>
+                                            <input type="text" className="form-control" placeholder="E-mail" />
                                         </div>
                                     </div>
                                 </div>
+
                                 <div className="row">
-                                    <div className="col-sm-8">
+                                    <div className="col-sm-4">
                                         <div className="inputfieldpadding">
-                                            <label className="text-left">Address</label>
-                                            <textarea className="form-control" style={{ height: 60, }} placeholder=""></textarea>
+                                            <label className="text-left"></label>
+                                            <textarea className="form-control" style={{ height: 60, }} placeholder="Address"></textarea>
                                         </div>
                                     </div>
-                                    <div className="col-sm-4">
+                                   
+                                    <div className="col-sm-2">
                                         <div className="inputfieldpadding">
                                             <label className="text-left">Gender</label>
                                             <select className="form-control m-bot15">
@@ -49,23 +47,8 @@ function ReservedBy() {
                                             </select>
                                         </div>
                                     </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-sm-6">
-                                        <div className="inputfieldpadding">
-                                            <label className="text-left">E-mail</label>
-                                            <input type="text" className="form-control" placeholder="" />
-                                        </div>
-                                    </div>
-                                    <div className="col-sm-6">
-                                        <div className="inputfieldpadding">
-                                            <label className="text-left">Phone</label>
-                                            <input type="text" className="form-control" placeholder="" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-sm-4">
+
+                                    <div className="col-sm-3">
                                         <div className="inputfieldpadding">
                                             <label className="text-left">Country</label>
                                             <select className="form-control m-bot15">
@@ -75,7 +58,7 @@ function ReservedBy() {
                                             </select>
                                         </div>
                                     </div>
-                                    <div className="col-sm-4">
+                                    <div className="col-sm-3">
                                         <div className="inputfieldpadding">
                                             <label className="text-left">State</label>
                                             <select className="form-control m-bot15">
@@ -85,16 +68,7 @@ function ReservedBy() {
                                             </select>
                                         </div>
                                     </div>
-                                    <div className="col-sm-4">
-                                        <div className="inputfieldpadding">
-                                            <label className="text-left">Zone</label>
-                                            <select className="form-control m-bot15">
-                                                <option>Option 1</option>
-                                                <option>Option 2</option>
-                                                <option>Option 3</option>
-                                            </select>
-                                        </div>
-                                    </div>
+                                 
                                 </div>
 
                             
@@ -105,38 +79,36 @@ function ReservedBy() {
                         <div>
                             <div className="col-sm-12">
                                 <div>
-                                    <h4 className="label"> COMMENTS </h4>
+                                    <h4 className='text-start'> Comments </h4>
                                 </div>
                                 <div className="column">
                                     <div className="row">
                                         <div className="col-sm-12">
-                                            <div  className="inputfieldpadding">
+                                            {/* <div  className="inputfieldpadding"> */}
                                                 <label className="text-left">Special Instructions/Comments</label>
                                                 <input type="text" className="form-control" placeholder="" />
-                                            </div>
+                                            {/* </div> */}
                                         </div>
                                     </div>
-                                    <div className="row">
-                                        <div className="col-sm-12">
-                                            <div className="inputfieldpadding">
-                                                <label className="text-left">Show Discounts</label>
+                                    
                                                 <div className="row">
                                                     <div className="col-sm-6">
+                                                        <label className="text-left">Apply Discount</label>
                                                         <input type="text" className="form-control shw_dsct_lft" defaultValue="GENERAL" />
                                                     </div>
                                                     <div className="col-sm-6">
+                                                        <label className="text-left">Discount Amount</label>
                                                         <input type="text" className="form-control shw_dsct_rgt" defaultValue="'100 RS" />
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                                
+                                 
                         
                                 </div>
 
                             </div>
                         </div>
-                        </ul>
+                        {/* </ul> */}
 
                     </div>
 
