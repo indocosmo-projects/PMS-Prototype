2d1ed91f9eeceabe885438b07b1efd6275b9a220
indodesigner <webdesigner@indocosmo.com>
2022-10-17 15:16:48 +0530

    edits

--- src/component/NewReservation/ReservedBy.js
+++ src/component/NewReservation/ReservedBy.js
@@ -102,8 +102,6 @@ function ReservedBy() {
                                                     </div>
                                                 </div>
                                                 
-                                 
-                        
                                 </div>
 
                             </div>
