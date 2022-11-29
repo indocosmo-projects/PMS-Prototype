fd2e19b0392d5c6f54cd431537ee6cfa56eaf9b6
mbpalat <mithunbpalat1993@gmail.com>
2022-11-29 13:00:45 +0530

    tax modal

--- src/component/Tax/Tax.js
+++ src/component/Tax/Tax.js
@@ -1,8 +1,79 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import TaxModal from '../Tax/TaxModal';
+import './Tax.css';
 
 function Tax() {
+    const details =  
+    [
+    {id: 1, code:'NO-TAX',name :'No Tax',indicator:'*',description:'No Tax'},
+    {id: 2, code:'NO-TAX',name :'No Tax',indicator:'*',description:'No Tax'},
+    {id: 3, code:'NO-TAX',name :'No Tax',indicator:'*',description:'No Tax'},
+    {id: 4, code:'NO-TAX',name :'No Tax',indicator:'*',description:'No Tax'},
+    {id: 5, code:'NO-TAX',name :'No Tax',indicator:'*',description:'No Tax'},
+
+   ];
+
+
+    const tdata = () =>{
         return(
-            <div></div>
+                <tbody>
+                    {
+                    details.map(detail => <tr key={detail.id} >
+                        <td className="left">{detail.code}</td>
+                        <td className="left">{detail.name}</td>
+                        <td className="left">{detail.indicator}</td>
+                        <td className="left">{detail.description}</td>
+                    </tr>
+                    )}
+                    
+                </tbody>
+        );
+    }
+
+
+
+        return(
+            <div>
+                <div className="section">
+                    <div className="container-fluid formcontent">
+                        {/* ----- Heading ------ */}
+                        <div className='sticky-div'>
+                            <h3 className="header"><i className="bi bi-calendar2-plus-fill me-2"></i>Tax</h3>
+                        </div>
+
+                           <div className="row">
+                            <div className="col-sm-6">
+                                <div className="input-group mb-3 ">
+                                            <input type="text" className="form-control" placeholder="Search..." />
+                                            <button className="btn btn-dark px-4" title="Search" ><i className ="bi bi-search"></i></button>
+                                    </div>
+                            </div>
+
+                                    <div className="col-sm-6">
+                                        <div className="mb-3 ">
+                                        <Link ><button className="btn btn-primary" title="New Reservation" style={{float : 'right'}}><i className ="bi bi-plus-circle me-2"></i><TaxModal/></button></Link>
+                                        </div>
+                                    </div>
+                            </div>
+
+                        <table className="table table-striped request_table table-bordered">
+                            <thead >
+                                <tr>
+                                    <th className="thstyle left">Code</th>
+                                    <th className="thstyle left">Name</th>
+                                    <th className="thstyle left">Indicator</th>
+                                    <th className="thstyle left">Description</th>
+                                </tr>
+                            </thead>
+                            {tdata()}
+
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+
         );
 }
  
---
+++ src/component/Tax/TaxModal.js
@@ -0,0 +1,121 @@
+import React,{ useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import './Tax.css';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
+function TaxModal() {
+
+const [show, setShow] = useState(false);
+
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+
+return (
+  <>
+    <Button variant="primary" onClick={handleShow}>
+     New 
+    </Button>
+
+    <Modal
+      show={show}
+      onHide={handleClose}
+      backdrop="static"
+      keyboard={false}
+      style={{marginTop: 0}}
+    >
+      <Modal.Header className="bg-info" closeButton>
+        <Modal.Title >New User Group</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+      <Tabs
+                   defaultActiveKey="general"
+                    id="justify-tab-example"
+                    className="mt-4 mb-4 status-tab"
+                    justify >
+                    <Tab className='tab-content-bg tab-bg' eventKey="general" title={<span><i className="bi bi-house-door-fill me-2"/>General</span>}>
+             
+                    <table>
+                        <tr>
+                            <td className="thwidth left">Code <span className="asterik">*</span></td>
+                            <td className="thwidth left"> <input type="text" className="form-control" id="pwd"  /></td>
+                        </tr>
+                        <tr>
+                            <td className="thwidth left">Name <span className="asterik">*</span></td>
+                            <td className="thwidth left"> <input type="text" className="form-control" id="pwd"  /></td>
+                        </tr>
+                        <tr>
+                            <td className="thwidth left">Description <span className="asterik">*</span></td>
+                            <td className="thwidth left"> 
+                              <input type="text" className="form-control" id="pwd"  />
+                            </td>
+                        </tr>
+                  
+                        <tr>
+                            <td className="thwidth left">Indicator <span className="asterik">*</span></td>
+                            <td className="thwidth left"> 
+                                <input type="text" className="form-control" id="pwd"  />
+                            </td>
+                        </tr>
+                      </table>
+
+                    </Tab>
+                    <Tab className='tab-content-bg tab-bg' eventKey="detail" title={<span><i className="bi bi-house-door-fill me-2"/>Details</span>}>
+
+                    <div >
+                            <div className='left bg-info p-1 rborder'>
+                                <h5>Tax Settings</h5>
+                            </div>
+                    </div>
+                    <table className="table table-responsive">
+                        <tr>
+                            <td className="thwidth left  text-dark">Applicable From </td>
+                            <td className="thwidth left text-dark">CGST(%) </td>
+                            <td className="thwidth left text-dark">SGST(%)</td>
+                            <td className="thwidth left text-dark">Cess(%) </td>
+                            <td className="thwidth left text-dark"> </td>
+                            <td className="thwidth left  text-dark bg-secondary"><i className="bi bi-plus me-2" style={{fontSize : 24}}></i> </td>
+                        </tr>
+                        <tr>
+                            <td className="thwidth left"> 
+                                <div class="form-group">
+                                  <select class="form-control" id="sel1">
+                                    <option>GENERAL DISCOUNT</option>
+                                    <option>PLAN BASED DISCOUNT</option>
+                                  </select>
+                                </div>
+                            </td>
+                            <td className="thwidth left">
+                              <input type="text" className="form-control" id="pwd"  />
+                            </td>
+                            <td className="thwidth left">
+                              <input type="text" className="form-control" id="pwd"  />
+                            </td>
+                            <td className="thwidth left">
+                              <input type="text" className="form-control" id="pwd"  />
+                            </td>
+                            <td className="thwidth left">
+                            </td>
+                            <td className="thwidth left">
+                                  <i className="bi bi-recycle me-2" style={{fontSize : 24}}></i> 
+                            </td>
+                        </tr>
+                    </table>
+             </Tab>
+          </Tabs>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={handleClose}>Save</Button>
+        <Button variant="danger" onClick={handleClose}>Reset</Button>
+        <Button variant="secondary" onClick={handleClose}>
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  </>
+);
+}
+
+export default TaxModal;
