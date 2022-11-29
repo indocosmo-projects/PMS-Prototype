3a7099e704ac7111d15c6db7bb5370990625ae33
mbpalat <mithunbpalat1993@gmail.com>
2022-11-29 17:39:51 +0530

    Room Type

--- src/component/RoomType/RoomType.js
+++ src/component/RoomType/RoomType.js
@@ -1,8 +1,86 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import RoomTypeModal from '../RoomType/RoomTypeModal';
 
 function RoomType() {
+
+    const details =  
+    [
+    {id: 1, code:'CLA',name :'Classic',description:'',single:'X',double:'X',triple:'X',quad:'X'},
+    {id: 2, code:'CLA',name :'Classic',description:'',single:'X',double:'X',triple:'X',quad:'X'},
+    {id: 3, code:'CLA',name :'Classic',description:'sd',single:'X',double:'X',triple:'X',quad:'X'},
+    {id: 4, code:'CLA',name :'Classic',description:'',single:'X',double:'X',triple:'X',quad:'X'},
+    {id: 5, code:'CLA',name :'Classic',description:'',single:'X',double:'X',triple:'X',quad:'X'},
+    {id: 6, code:'CLA',name :'Classic',description:'',single:'X',double:'X',triple:'X',quad:'X'},
+    
+];
+
+
+    const tdata = () =>{
         return(
-            <div></div>
+                <tbody>
+                    {
+                    details.map(detail => <tr key={detail.id} >
+                        <td className="left">{detail.code}</td>
+                        <td className="left">{detail.name}</td>
+                        <td className="left">{detail.description}</td>
+                        <td className="left">{detail.single}</td>
+                        <td className="left">{detail.double}</td>
+                        <td className="left">{detail.triple}</td>
+                        <td className="left">{detail.quad}</td>
+                     
+                    </tr>
+                    )}
+                    
+                </tbody>
+        );
+    }
+
+
+        return(
+            <div>
+                <div className="section">
+                    <div className="container-fluid formcontent">
+                        {/* ----- Heading ------ */}
+                        <div className='sticky-div'>
+                            <h3 className="header"><i className="bi bi-calendar2-plus-fill me-2"></i>Room Type</h3>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-sm-6">
+                                <div className="input-group mb-3 ">
+                                            <input type="text" className="form-control" placeholder="Search..." />
+                                            <button className="btn btn-dark px-4" title="Search" ><i className ="bi bi-search"></i></button>
+                                    </div>
+                            </div>
+
+                            <div className="col-sm-6">
+                                <div className="mb-3 ">
+                                <Link ><button className="btn btn-primary" title="New Reservation" style={{float : 'right'}}><i className ="bi bi-plus-circle me-2"></i><RoomTypeModal/></button></Link>
+                                </div>
+                            </div>
+                        </div>
+
+                          <table className="table table-striped request_table table-bordered">
+                            <thead >
+                                <tr>
+                                    <th className="thstyle left">Code</th>
+                                    <th className="thstyle left">Name</th>
+                                    <th className="thstyle left">Description</th>
+                                    <th className="thstyle left">Single Occ</th>
+                                    <th className="thstyle left">Double Occ</th>
+                                    <th className="thstyle left">Triple Occ</th>
+                                    <th className="thstyle left">Quad Occ</th>
+                                </tr>
+                            </thead>
+                            {tdata()}
+
+                        </table>
+
+
+                    </div>
+                </div>
+            </div>
         );
 }
  
---
+++ src/component/RoomType/RoomTypeModal.js
@@ -0,0 +1,83 @@
+import React,{ useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import './RoomType.css';
+
+function RoomTypeModal() {
+
+const [show, setShow] = useState(false);
+
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+
+return (
+  <>
+    <Button variant="primary" onClick={handleShow}>
+      New
+    </Button>
+
+    <Modal
+      show={show}
+      onHide={handleClose}
+      backdrop="static"
+      keyboard={false}
+      style={{marginTop: 10}}
+    >
+      <Modal.Header className="bg-info" closeButton>
+        <Modal.Title >Room Type</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+          <table>
+              <tr>
+                  <td className="thwidth left">Code <span className="asterik">*</span></td>
+                  <td className="thwidth left"> <input type="text" class="form-control" id="pwd"  /></td>
+    
+                  <td className="thwidth left">Name <span className="asterik">*</span></td>
+                  <td className="thwidth left"> <input type="text" class="form-control" id="pwd"  /></td>
+              </tr>
+              <tr>
+                  <td className="thwidth left">Overbooking % </td>
+                  <td className="thwidth left"> <input type="text" class="form-control" id="pwd"  /></td>
+    
+                  <td className="thwidth left">Display order </td>
+                  <td className="thwidth left"> <input type="text" class="form-control" id="pwd"  /></td>
+              </tr>
+              </table>
+              <table>
+              <tr>
+                  <td className="thwidth left">Description</td>
+                  <td className="thwidth left"><input type="text" class="form-control " id="pwd"  /></td>
+                  <td className="thwidth left"></td>
+                  <td className="thwidth left"></td>
+              </tr>
+          </table>
+          <div >
+                    <div className='left bg-info p-1 rborder'>
+                        <h5>Applicable occupancies</h5>
+                    </div>
+
+                            <table>
+                                <tr>
+                                    <td className="thwidth left">Single <input type="checkbox" id="chk" className="chk"/> </td>
+                                    <td className="thwidth left">Double <input type="checkbox" id="chk" className="chk"/> </td>
+                                    <td className="thwidth left">Triple <input type="checkbox" id="chk" className="chk"/> </td>
+                                    <td className="thwidth left">Quad <input type="checkbox" id="chk" className="chk"/> </td>
+                                </tr>
+                            </table>
+                        </div>
+
+
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={handleClose}>Save</Button>
+        <Button variant="danger" onClick={handleClose}>Reset</Button>
+        <Button variant="secondary" onClick={handleClose}>
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  </>
+);
+}
+
+export default RoomTypeModal;
