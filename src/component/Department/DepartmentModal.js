9ee918d93bc3bd77ada2219ca22134975913ceb9
mbpalat <mithunbpalat1993@gmail.com>
2022-11-29 10:03:03 +0530

    department

---
+++ src/component/Department/DepartmentModal.js
@@ -0,0 +1,59 @@
+import React,{ useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import './Department.css';
+
+function DepartmentModal() {
+
+const [show, setShow] = useState(false);
+
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+
+return (
+  <>
+    <Button variant="primary" onClick={handleShow}>
+    New
+    </Button>
+
+    <Modal
+      show={show}
+      onHide={handleClose}
+      backdrop="static"
+      keyboard={false}
+      style={{marginTop: 100}}
+    >
+      <Modal.Header className="bg-info" closeButton>
+        <Modal.Title >New User Group</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+          <table>
+              <tr>
+                  <td className="thwidth left">Code <span className="asterik">*</span></td>
+                  <td className="thwidth left"> <input type="text" className="form-control" id="pwd"  /></td>
+              </tr>
+              <tr>
+                  <td className="thwidth left">Name <span className="asterik">*</span></td>
+                  <td className="thwidth left"> <input type="text" className="form-control" id="pwd"  /></td>
+              </tr>
+              <tr>
+                  <td className="thwidth left">Description <span className="asterik">*</span></td>
+                  <td className="thwidth left"> 
+                    <input type="text" className="form-control" id="pwd"  />
+                  </td>
+              </tr>
+          </table>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={handleClose}>Save</Button>
+        <Button variant="danger" onClick={handleClose}>Reset</Button>
+        <Button variant="secondary" onClick={handleClose}>
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  </>
+);
+}
+
+export default DepartmentModal;
